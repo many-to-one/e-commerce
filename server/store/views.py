c1688689b1a1213213441c5d720c5e112b33fa0b
many-to-one <xllxlex@gmail.com>
2025-02-11 13:50:25 +0100

    add images to the gallery logic and return to the product serialized data

--- server/store/views.py
+++ server/store/views.py
@@ -7,7 +7,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
-from .serializers import ProductSerializer, CategorySerializer
+from .serializers import ProductSerializer, CategorySerializer, GallerySerializer
 from .models import Category, Product
 
 
@@ -31,5 +31,10 @@ class ProductDetailsView(APIView):
 
     def get(self, request, slug):
         product = get_object_or_404(Product, slug=slug)
+        gallery = product.gallery()
         serializer = ProductSerializer(product)
-        return Response(serializer.data)
+        gallery_serializer = GallerySerializer(gallery, many=True)
+        return Response({
+            "product": serializer.data,
+            "gallery": gallery_serializer.data,
+        })
