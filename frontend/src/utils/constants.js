2307a07faab37f98a9cba2a42beaeb59fb5eec93
root <root@Alex.localdomain>
2025-11-08 20:17:16 +0100

    changes for production system

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-// export const API_BASE_URL = 'https://api.kidnetic.pl/';
-export const API_BASE_URL = 'http://127.0.0.1:8100/';
+export const API_BASE_URL = 'https://api.kidnetic.pl/';
+// export const API_BASE_URL = 'http://127.0.0.1:8100/';
 export const PAYPAL_CLIENT_ID = 'test'
 export const SERVER_URL = 'https://api.kidnetic.pl/'
