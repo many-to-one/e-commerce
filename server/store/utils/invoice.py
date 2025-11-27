8e23ea2bfe67431f6da9ad0d1693b3742131ad19
many-to-one <xllxlex@gmail.com>
2025-11-27 19:06:26 +0100

    debug invoice inline

--- server/store/utils/invoice.py
+++ server/store/utils/invoice.py
@@ -214,7 +214,7 @@ def generate_invoice_allegro(invoice, vendor, user, buyer_info, products):  # ta
     # Buyer 
     c.drawString(11 * cm, height - 4 * cm, "Nabywca:")
     c.drawString(11 * cm, height - 4.5 * cm, str(buyer_info.get('name') or ""))
-    c.drawString(11 * cm, height - 5 * cm, f"ul. {buyer_info.get('street','')} {buyer_info.get('zipCode','')} {buyer_info.get('city','')}")
+    c.drawString(11 * cm, height - 5 * cm, f"ul. {buyer_info.get('street','')}, {buyer_info.get('zipCode','')} {buyer_info.get('city','')}")
     c.drawString(11 * cm, height - 5.5 * cm, f"NIP {buyer_info.get('taxId','')}")
 
     # Table setup
