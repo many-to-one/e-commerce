8561735bc739a5425c51df3a0575ddf94c2af35a
many-to-one <xllxlex@gmail.com>
2026-01-07 19:40:08 +0100

    product admin started and ended all-o offers

--- server/store/admin.py
+++ server/store/admin.py
@@ -222,6 +222,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = [
         'modified_hidden', 
         'allegro_started_at',
+        'allegro_ended_at',
         'sku', 
         'vendor_checkboxes', 
         'product_image', 
@@ -751,7 +752,7 @@ class ProductAdmin(admin.ModelAdmin):
                     started_at = offer.get("publication", {}).get("startedAt")
                     watchers = offer.get("stats", {}).get("watchersCount")
                     visits = offer.get("stats", {}).get("visitsCount")
-
+                    ended_at = offer.get("publication", {}).get("endedAt")
 
 
                     if not product:
@@ -772,6 +773,7 @@ class ProductAdmin(admin.ModelAdmin):
                     product.allegro_watchers = watchers
                     product.allegro_visits = visits
                     product.allegro_started_at = started_at
+                    product.allegro_ended_at = ended_at
 
                     # Jeśli oferta jest aktywna → aktualizujemy dodatkowe dane
                     if status == "ACTIVE":
@@ -803,54 +805,8 @@ class ProductAdmin(admin.ModelAdmin):
                         "allegro_watchers",
                         "allegro_visits",
                         "allegro_started_at",
+                        "allegro_ended_at",
                     ])
-
-
-                # for offer in offers:
-                #     # print(f' ################### "offer" ################### ', offer)
-
-                #     id = offer.get("id")
-                #     external = offer.get("external")
-                #     if not external:
-                #         continue 
-
-                #     sku = external.get("id")
-                #     status = offer.get("publication", {}).get("status")
-                #     # print(f' ################### "vendor" ################### ', vendor.name)
-                #     # print(f' ################### "status" ################### ', status)                
-                #     product = product_map.get(sku)
-                #     # print(f' ################### "id-sku" ################### ', id, sku)
-
-                #     if not product:
-                #         continue
-                    
-                #     if status == "ACTIVE":
-                #         count += 1
-                #         print(f' ################### "ACTIVE" ################### {sku} ----- ', product.sku)
-                #         product.title = offer.get("name", product.title)
-    
-                #         entry = {"vendor": vendor.name, "product_id": id}
-
-                #         if entry not in product.allegro_ids:
-                #             product.allegro_ids.append(entry)
-
-                #         product.allegro_in_stock = True
-                #         price_brutto = Decimal(str(offer.get("sellingMode", {}).get("price", {}).get("amount", "0")))
-                #         price_netto = (price_brutto / Decimal("1.23")).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
-
-                #         product.price = price_netto
-                #         product.price_brutto = price_brutto
-                #         print(f' ################### "count" ################### ', count)
-                #         product.allegro_status = status
-                #         product.save(update_fields=["title", "allegro_ids", "allegro_in_stock", "allegro_status", "price", "price_brutto",])
-                #     else:
-                #         # print(f' ################### else "status" ################### ', status) 
-                #         product.allegro_in_stock = False
-                #         product.allegro_status = status
-                #         product.save(update_fields=["title", "allegro_ids", "allegro_in_stock", "allegro_status", "price", "price_brutto",])
-
-                #     # product.allegro_status = status
-                #     # product.save(update_fields=["title", "allegro_ids", "allegro_in_stock", "allegro_status", "price", "price_brutto",])
 
                 self.message_user(request, "Twoje oferty zostały zaktualizowane", level="success")
 
