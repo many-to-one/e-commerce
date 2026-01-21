39058ac54ba1d6e133058b8013cb5a3cb0632f75
many-to-one <xllxlex@gmail.com>
2026-01-21 19:13:09 +0100

    invoices report with pdf

--- admin@45.141.3.185/admin/js/my.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("change", function(e) {
-    if (e.target.classList.contains("vendor-toggle")) {
-        console.log("kliknięto checkbox", e.target.dataset);
-    }
-});
-
-console.log("my.js załadowany");
